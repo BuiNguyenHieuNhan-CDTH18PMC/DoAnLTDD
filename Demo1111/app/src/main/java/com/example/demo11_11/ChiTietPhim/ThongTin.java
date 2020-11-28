019ddf64c85b8a2eb9629bf74ddeaeedbf8dcfc1
BuiNguyenHieuNhan-CDTH18PMC <73632249+HieuNhan2605@users.noreply.github.com>
2020-11-29 01:26:27 +0700

    add trailer

--- Demo1111/app/src/main/java/com/example/demo11_11/ChiTietPhim/ThongTin.java
+++ Demo1111/app/src/main/java/com/example/demo11_11/ChiTietPhim/ThongTin.java
@@ -1,20 +1,32 @@
 package com.example.demo11_11.ChiTietPhim;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.example.demo11_11.R;
 
 
 public class ThongTin extends Fragment {
+    MediaController media;
+    int position = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thong_tin, container, false);
+        View view = inflater.inflate(R.layout.fragment_thong_tin, container, false);
+        VideoView videoView = (VideoView)view.findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+ getActivity().getPackageName()+"/"+R.raw.trailer_tiectrangmau);
+        videoView.setVideoURI(uri);
+        videoView.start();
+        return view;
     }
 }
